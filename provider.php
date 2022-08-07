caf15812e7426fa34abe5cf18ddf3f3ab034d141
Manoj <84076478+Manoj546@users.noreply.github.com>
2022-08-07 10:32:43 +0530

    provider

--- provider.php
+++ provider.php
@@ -5,7 +5,7 @@ error_reporting(0);
 
 if (isset($_POST['submit'])) {
 	$Provider_id = $_POST['Provider_id'];
-	$Organisation name = $_POST['Organisation name'];
+	$Organisation = $_POST['Organisation name'];
     $restr = strtoupper($username);
 	$password = $_POST['password'];
     $sql = "SELECT * FROM scholarships WHERE scholarships_name='$restr'";
@@ -38,20 +38,20 @@ if (isset($_POST['submit'])) {
         <meta name="viewport" content="width=device-width, initial-scale=1.0" />
         <link rel="stylesheet" href="css/provider.css" />
         <script src="https://kit.fontawesome.com/a8ab30b9ae.js" crossorigin="anonymous"></script>
-        <title>Schloarrship providers form:</title>
+        <title>Schloarship providers form</title>
       </head>
       
       <body>
         <div class="container">
-            <header>Schloarship providers</header>
-            <form action="#">
+            <header>Scholarship Details</header>
+            <form action="" method="POST" >
                 <div class="form first">
                     <div class=" details schloarship">
-                        <span class="title" >Enter Schloarship details here:</span>
+                        <span class="title" >Enter schloarship details here:</span>
                         <div class="fields">
                             <div class="input-field">
                                 <label>Provider_id :</label>
-                                <input type="text" placeholder="Enter Provider_id" name="provider" value="<?php echo $provider; ?>" required >
+                                <input type="text" placeholder="" name="provider" value="<?php echo $provider; ?>" required >
                             </div>
                             <div class="input-field">
                                 <label>Organisation name <span class="redstar">*</span> :</label>
@@ -67,7 +67,16 @@ if (isset($_POST['submit'])) {
                             </div>
                             <div class="input-field">
                                 <label>Eligibility :</label>
-                                <input type="text" placeholder="" required>
+                                <select id="cars" name="type">
+                                    <option value="">--Select--</option>
+                                    <option value="Engineering">Engineering</option>
+                                    <option value="Medical">Medical</option>
+                                    <option value="Business">Business</option>
+                                    <option value="Sports">Sports</option>
+                                    <option value="Study Abroad">Study Abroad</option>
+                                    <option value="Management">Management</option>
+                                    <option value="Law">Law</option>
+                                </select>
                             </div>
                             <div class="input-field">
                                 <label>Benefits of the schloarship :</label>
